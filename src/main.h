f8b74effe27fc6f6f23d6b06d46f0e385a9f2ae3
J-Jankowski <jjankowski@live.ca>
2017-06-14 00:49:54 -0700

    starting to test exti

--- src/main.h
+++ src/main.h
@@ -10,7 +10,8 @@
 
 // *****************************DEFINITIONS******************************
 
-
+#define	MYTIM4_PERIOD ((uint16_t)0x0690)				// debouncing timer runs for 10ms
+#define myTIM4_PRESCALER ((uint32_t)0x3138)				// so need to count to 1680000/1000=1680=0x0690
 #define myTIM2_PRESCALER ((uint16_t)0x03E8)				//want ADC to run every 75ms =13.3Hz board is at 168MHz prescale by 1000
 #define myTIM2_PERIOD ((uint32_t)0x3138)				//so need to count to count to 12600=0x3138 gona change for testing
 #define NUM_CHANNELS 13
